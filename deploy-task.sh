a1e7a4130410246a2eaa92118910426dc60c8670
Johannes Engelke | hybris <johannes.engelke@hybris.com>
2015-07-02 09:38:08 +0200

    Stop stack creation

--- deploy-task.sh
+++ deploy-task.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 
-aws cloudformation create-stack \
-  --stack-name ${STACK_NAME} --template-body file://jumpbox-template/template.json \
-  --parameters \
-  ParameterKey=BoshNetworkCidrBlock,ParameterValue=${BOSH_NETWORK_CIDR_BLOCK} \
-  ParameterKey=JumpboxNetworkCidrBlock,ParameterValue=${JUMPBOX_NETWORK_CIDR_BLOCK} \
-  ParameterKey=VPCName,ParameterValue=${STACK_NAME} \
-  ParameterKey=VPCCidrBlock,ParameterValue=${VPC_CIDR_BLOCK}
+# aws cloudformation create-stack \
+#   --stack-name ${STACK_NAME} --template-body file://jumpbox-template/template.json \
+#   --parameters \
+#   ParameterKey=BoshNetworkCidrBlock,ParameterValue=${BOSH_NETWORK_CIDR_BLOCK} \
+#   ParameterKey=JumpboxNetworkCidrBlock,ParameterValue=${JUMPBOX_NETWORK_CIDR_BLOCK} \
+#   ParameterKey=VPCName,ParameterValue=${STACK_NAME} \
+#   ParameterKey=VPCCidrBlock,ParameterValue=${VPC_CIDR_BLOCK}
 
 
 
 
-  aws cloudformation list-stacks
+aws cloudformation list-stacks
